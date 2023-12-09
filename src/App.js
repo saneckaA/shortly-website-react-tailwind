44d4e2e976a3b04c2e8d3a7d2a3e3d1e430cbd78
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-12-09 11:16:35 +0100

    imported components to the app file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import StatsSection from './StatsSection';
+import Form from './Form';
+import MainSection from './MainSection';
+import Navigation from './Navigation';
+import FeaturesSection from './FeaturesSection';
+import CTASection from './CTASection';
+import Footer from './Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navigation />
+      <MainSection />
+      <Form />
+      <StatsSection />
+      <FeaturesSection />
+      <CTASection />
+      <Footer />
     </div>
   );
 }
